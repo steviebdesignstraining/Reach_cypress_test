3f51bc6a5d67f2d7c1ed4cb59c406fdfbbfef99b
Stephen Bennett <47852623+steviebdesignstraining@users.noreply.github.com>
2020-11-06 07:56:22 +0000

    Initial commit

---
+++ cypress/integration/examples/capture_company_details.js
@@ -0,0 +1,33 @@
+describe ('QA Engineer - Reach - Capture company details on company names - Home screen', () => {
+    it('Should load pharmaceutical company names', () => {
+        cy.visit('https://www.medicines.org.uk/emc/browse-companies')
+    })
+    it('Should check for header on page', () => {
+        cy.get('h1').should('be.visible').contains('Browse pharmaceutical company names')
+    })
+    it('Click on first company, check page and capture company details', () => {
+        cy.get('.ieleft > ul > :nth-child(9) > .key').click()
+        cy.get('h1').should('be.visible').contains('Alan Pharmaceuticals')
+        cy.downloadFile('https://www.medicines.org.uk/emc/images/logo/3980','mydownloads','logo.jpg')
+        cy.writeFile('Alan_Pharmaceutical.json', { Name: 'Alan Pharmaceuticals', address: 'Highgate Business Centre, 33 Greenwood Place, London, London, NW5 1LB, UK', telephone: '0207 284 2887', email: 'info@alanpharma.com', website: 'http://www.alanpharmaceuticals.com', fax: '0207 267 9841', directline: '0207 284 2887'})
+        cy.readFile('Alan_Pharmaceutical.json').its('Name',).should('eq', 'Alan Pharmaceuticals')
+        cy.get('.nav>:nth-child(3)>a').click()
+    })
+    it('Click on second company, check page and capture company details', () => {
+        cy.get('.ieright>ul>:nth-child(10)>.key').click()
+        cy.get('h1').should('be.visible').contains('Aspen')
+        cy.downloadFile('https://www.medicines.org.uk/emc/images/logo/2955','mydownloads','logo2.jpg')
+        cy.writeFile('Aspen.json', { Name: 'Aspen', address: '3016 Lake Drive, Citywest Business Campus, Dublin 24, Ireland', telephone: '+ 44 1748 828 391', email: 'info@alanpharma.com', directline: '0800 0087 392'})
+        cy.readFile('Aspen.json').its('Name',).should('eq', 'Aspen')
+        cy.get('.nav>:nth-child(3)>a').click()
+    })
+    it('Click on second company, check page and capture company details', () => {
+        cy.get(':nth-child(19)>a').click()
+        cy.get('.ieright>ul>:nth-child(6)>.key').click()
+        cy.get('h1').should('be.visible').contains('RPH Pharmaceuticals AB')
+        cy.writeFile('RPH_Pharmaceuticals_AB.json', { Name: 'RPH Pharmaceuticals AB', address: 'Lagervagen 7, Jordbro, Sweden', telephone: '+44 (0)845 023 0467', email: 'customer.care@recipharm.com', medical_Information_email: 'safety@elc-group.com', directline: '+44(0)845 023 0467', stock_Availability: '+44(0)845 023 0467'})
+        cy.readFile('RPH_Pharmaceuticals_AB.json').its('Name',).should('eq', 'RPH Pharmaceuticals AB')
+        cy.get('.nav>:nth-child(3)>a').click()
+    })
+})
+
